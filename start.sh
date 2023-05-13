6def640141c89b3c2673a5e65ceb195b444de653
Maxim Lityagin <sidecuter@outlook.com>
2023-05-13 11:28:31 +0300

    start.sh: add folder exists check

--- start.sh
+++ start.sh
@@ -4,8 +4,12 @@ export SDOP_DB_NAME= # db name
 export SDOP_DB_USER= # user name
 export SDOP_DB_PASS= # user pass
 
-mkdir database
-mkdir trash
+if [ ! -d "database" ]; then
+  mkdir database
+fi
+if [ ! -d "trash" ]; then
+  mkdir trash
+fi
 
 docker compose build
 docker compose up
