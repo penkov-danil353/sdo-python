cbfae45001733f9f20b8716dc6109cfb2d64bbd4
Maxim Lityagin <sidecuter@outlook.com>
2023-05-13 14:31:12 +0300

    docker-compose.yml: remove external volume and change it on local start.sh: remove database folder creation

--- start.sh
+++ start.sh
@@ -4,9 +4,6 @@ export SDOP_DB_NAME= # db name
 export SDOP_DB_USER= # user name
 export SDOP_DB_PASS= # user pass
 
-if [ ! -d "database" ]; then
-  mkdir database
-fi
 if [ ! -d "trash" ]; then
   mkdir trash
 fi
