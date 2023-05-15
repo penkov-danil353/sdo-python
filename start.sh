8068dcaaecc29b0e4b147e9ca0841c5cade04427
Maxim Lityagin <sidecuter@outlook.com>
2023-05-15 14:59:16 +0300

    docker-compose.yml: make external vaults

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ export SDOP_DB_NAME= # db name
 export SDOP_DB_USER= # user name
 export SDOP_DB_PASS= # user pass
 
+docker volume create db-data
+docker volume create trash
+
 docker compose build
-docker compose up
+docker compose start
