62be17ed60b9825f6cff38e77af7698db5b7cb1b
Maxim Lityagin <sidecuter@outlook.com>
2023-06-17 13:51:00 +0300

    fix pipeline x5

--- modules/parse/parsewrapper.py
+++ modules/parse/parsewrapper.py
@@ -7,7 +7,10 @@ def calc_evaluation(evaluation: str) -> float:
     if platform.system() == "Windows":
         lib = ctypes.CDLL("./libparse.dll")
     else:
-        lib = ctypes.CDLL("libparse.so")
+        try:
+            lib = ctypes.CDLL("~/.local/lib/libparse.so")
+        except Exception as e:
+            lib = ctypes.CDLL("libparse.so")
     lib.calc_eval.argtypes = [ctypes.c_char_p, ]
     lib.calc_eval.restype = ctypes.c_double
     return lib.calc_eval(ctypes.c_char_p(evaluation.encode('utf-8')))
